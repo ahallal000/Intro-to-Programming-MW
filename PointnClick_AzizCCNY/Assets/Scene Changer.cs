e4446dd8a98e4fcfd6e119197055a87a211d9662
ahallal000 <ahallal000@citymail.cuny.edu>
2024-03-11 18:14:42 -0400

    Added Point and Click

---
+++ PointnClick_AzizCCNY/Assets/Scene Changer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public int sceneNumber;
+    //0 = StartScene
+    //1 = MainScene
+    //2 = EndScene
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneNumber == 0)
+        {
+            StartSceneControls();
+        } else if (sceneNumber == 1)
+        {
+            MainSceneControls();
+        } else if (sceneNumber == 2)
+        {
+            EndSceneControls();
+        }
+    }
+
+
+    public void StartSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    public void MainSceneControls()
+    {
+        //Debug.Log("main scene controls");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+
+    public void EndSceneControls()
+    {
+        //Debug.Log("end scene controls");
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+
+
+}
