dd22c243a53a8c18da2d101bcee0a2b887b9bb41
ahallal000 <ahallal000@citymail.cuny.edu>
2024-04-08 18:13:57 -0400

    added enemy and enemy movement

---
+++ 2D Sidescroller/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+
+
+    public int damage;
+    public PlayerController playerControllerScript;
+
+    public Transform[] patrolPoints;
+    public float moveSpeed = 3;
+    public int patrolDestination;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //currentHealth = currentHealth - 1;
+            //Debug.Log("Player Touched");
+
+            playerControllerScript.TakeDamage(damage);
+
+        }
+    }
+
+    private void EnemyMovement()
+    {
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+
+
+            if (Vector3.Distance(transform.position, patrolPoints[0].position) < 0.5)
+            {
+                patrolDestination= 1;
+            }
+
+
+        }
+
+        if (patrolDestination == 1)
+        {
+            //Debug.Log("change direction");
+
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+
+
+                if (Vector3.Distance(transform.position, patrolPoints[1].position) < 0.5)
+                {
+                    patrolDestination = 0;
+                }
+
+        }
+
+        
+    }
+
+}
